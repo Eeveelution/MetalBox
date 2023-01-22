9110adf1c810639efa38e83eaffe292228ef873b
Furball <33634370+Eeveelution@users.noreply.github.com>
2023-01-22 16:57:32 +0100

    for fucks sake why

--- simdjson.sh
+++
@@ -1,2 +0,0 @@
-wget https://raw.githubusercontent.com/simdjson/simdjson/master/singleheader/simdjson.h -O MetalBox/fastgltf/simdjson.h
-wget https://raw.githubusercontent.com/simdjson/simdjson/master/singleheader/simdjson.cpp -O MetalBox/fastgltf/simdjson.cpp
